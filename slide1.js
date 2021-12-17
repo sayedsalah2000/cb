078ae6c7c18f264f2dcb9d2e3949ee487818601c
arkadhost <83273378+arkadhost@users.noreply.github.com>
2021-12-17 03:17:34 +0200

    Create slide1.js

---
+++ slide1.js
@@ -0,0 +1,74 @@
+document.write(`<div class="card-wrap">
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/1.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/2.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/3.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/4.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/5.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/6.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/7.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/8.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/9.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/10.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/11.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+    <div class="card">
+        <div class="imgBox">
+            <img src="img/senjata/12.jpeg">
+        </div>
+        <div class="btn-claim" onclick="gass(this)">Claim</div>
+    </div>
+</div>`);
